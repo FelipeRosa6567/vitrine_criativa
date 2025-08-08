41a9149b9eeac5e5cd5d61f86ab7590472c2592c
leinadgp <leinadgp@gmail.com>
2025-08-08 15:37:52 -0300

    ajuste

--- src/containers/Livros/index.jsx
+++ src/containers/Livros/index.jsx
@@ -1,11 +1,19 @@
-import { ContainerMain } from "./styles"
+import { ContainerMain, ContainerLivros } from "./styles"
 
 function Livros() {
    
  
    return (
      <>
-       <ContainerMain>Livros teste</ContainerMain>
+       <ContainerMain>
+          
+            <h1 style={{ textAlign: "center" }}>Coleção de livros</h1>
+            <hr style={{ border: "none", height: "2px", backgroundColor: "#ffffff", width: "130%" }} />
+         <ContainerLivros>  
+            <img src="../../" alt="" />
+            
+        </ContainerLivros>
+       </ContainerMain>
      </>
    );
  }
--- src/containers/Livros/styles.js
+++ src/containers/Livros/styles.js
@@ -2,10 +2,17 @@ import styled from 'styled-components'
 
 export const ContainerMain = styled.div`
   display: flex;
+  flex-direction: column;
   height: 100vh;
   width: 100vw;
-  display: flex;
-  justify-content: space-between;
   align-items: center;
-  padding: 0 100px;
+  padding: 100px 100px;
+  background-color: transparente;
+`
+
+export const ContainerLivros = styled.div`
+  display: grid;
+  gap: 1rem; /* espaçamento entre colunas */
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  max-width: calc(4 * 250px + 3 * 1rem); /* limita a 4 colunas */
 `
